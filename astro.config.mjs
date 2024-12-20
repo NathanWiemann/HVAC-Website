af4cd608bc74385237c426406c58d23191f82dc3
NathanWiemann <129617832+NathanWiemann@users.noreply.github.com>
2024-12-19 19:29:38 -0500

    Still working on getting sitemap.xml to show

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import sitemap from 'astro-sitemap';
 
 export default defineConfig({
   site: 'https://berkscountyhvac.com',
